5d9c688ab3fbd0dc094a620d87a4763b6760e234
Dev Prashant <dev.prashant1901@gmail.com>
2026-02-06 11:29:28 +0530

    shop changes

--- src/routes/point.routes.js
+++ src/routes/point.routes.js
@@ -4,5 +4,6 @@ const v = require("../middleware/verifyToken");
 
 const c = require("../controllers/power");
 r.post("/update", v, c.updatePower);
+r.post("/upgrade", v, c.upgradePower);
 
 module.exports = r;
