686fa463eb7431beb86619b633dec8c50ed7e3e6
Dev Prashant <dev.prashant1901@gmail.com>
2026-05-04 00:49:06 +0530

    refactor: standardize API routes and consolidate point/power routes

--- src/routes/power.routes.js
+++
@@ -1,8 +0,0 @@
-const r = require("express").Router();
-const v = require("../middleware/verifyToken");
-const c = require("../controllers/point.controller");
-
-r.post("/upgrade", v, c.upgradePower);
-r.post("/aid", v, c.sendAid);
-
-module.exports = r;
